e7afc766c8f56b5ef900aaa50c342d1bbdab9936
TuniSenpao <robert-liefke@gmx.de>
2022-01-11 20:42:56 +0100

    fixed bugs and added some tests

--- cypress/integration/task02.spec.js
+++ cypress/integration/task02.spec.js
@@ -1,5 +1,17 @@
 context('Task 2', () => {
-    it('List all rooms', () => {
-        cy.visit('http://localhost:4200/')
+  it('Create a new room', () => {
+      cy.visit('http://localhost:4200/rooms');
+      cy.get('tr').then((len) => {
+          const initialCount = len.length;
+          cy.visit('http://localhost:4200/rooms/new');
+          cy.get('[name="name"').type('testName');
+          cy.get('[name="address"').type('testAddress');
+          cy.get('[name="capacity"').type(4);
+          cy.get('[name="features"').type('testFeatures');
+          cy.get('[type="submit"]').click();
+          cy.wait(2000);
+          cy.visit('http://localhost:4200/rooms');
+          cy.get('table').find('tr').should('have.length', initialCount+1);
     })
+  });
 })
--- cypress/integration/task03.spec.js
+++ cypress/integration/task03.spec.js
@@ -1,5 +1,18 @@
 context('Task 3', () => {
     it('Display a single room', () => {
-        cy.visit('http://localhost:4200/')
+        cy.visit('http://localhost:4200/rooms/new');
+        cy.get('[name="name"]').type('testName');
+        cy.get('[name="address"]').type('testAddress');
+        cy.get('[name="capacity"]').type(4);
+        cy.get('[name="features"]').type('testFeatures');
+        cy.get('[type="submit"]').click();
+        cy.wait(1000);
+
+        cy.visit('http://localhost:4200/rooms');
+        cy.get('[name="roomView"]').first().should('have.attr', 'href').then((href) => {
+            cy.get('[name="roomView"]').first().click();
+            cy.wait(1000);
+            cy.url().should('include', href);
+        })
     })
 })
